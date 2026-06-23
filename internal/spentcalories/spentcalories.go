0ceced72baa6363417940bf06d31c46d7f39445d
Swtnght <106587272+Swtnght@users.noreply.github.com>
2026-06-23 07:28:58 +0300

    minor fix

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -83,8 +83,7 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 			"Дистанция: %.2f км.\n"+
 			"Скорость: %.2f км/ч\n"+
 			"Сожгли калорий: %.2f\n",
-		types, duration.Hours(), float64(distance(step, height)), avgSpeed, callories
-	)
+		types, duration.Hours(), float64(distance(step, height)), avgSpeed, callories)
 	return report, nil
 }
 
